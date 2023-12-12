386a19a608671bba8ccbf39e493380c06d539b9a
GITKLM <152407061+GITKLM@users.noreply.github.com>
2023-12-12 11:02:18 +0400

    Update functions.php

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -7,6 +7,6 @@ function theme_enqueue_styles(){
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
-
+wp_nav_menu('Admin', 'menu', 'menu-item-1133','div', 'menu-{menu slug}-container')
 
 
