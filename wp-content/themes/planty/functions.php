d508855ae4f051571f60a729e818e5d36a1f1c27
GITKLM <152407061+GITKLM@users.noreply.github.com>
2023-12-13 13:58:32 +0400

    COmmande boutons quantité

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -1,5 +1,4 @@
 <?php
-
 // Action qui permet de charger des scripts dans notre thème
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles(){
@@ -7,13 +6,12 @@ function theme_enqueue_styles(){
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
-
-
-// function example_callback( $menu-title ) {
-    // if()
-//     return $menu-title;
-// }
-
-// add_filter( 'example_filter', 'example_callback' );
-
-
+function masquer_element_admin_non_connecte($items, $args) {
+    if ($args->theme_location == 'primary') {
+        if (is_user_logged_in ()) {
+            $items .= '<li id="menu-item-1134" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-1130 current_page_item menu-item-1134"><a class="menu-link" href="http://localhost:8888/Planty/wp-admin/index.php" aria-current="page">Admin</a></li>';
+        }
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'masquer_element_admin_non_connecte', 10, 2);
